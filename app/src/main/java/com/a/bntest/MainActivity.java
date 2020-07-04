60b50e0566153cec5fbab43b7b1221770ebd1543
dm2911 <m291119@ua.fm>
2020-07-04 16:09:35 +0300

    Ch All

--- app/src/main/java/com/a/bntest/MainActivity.java
+++ app/src/main/java/com/a/bntest/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
